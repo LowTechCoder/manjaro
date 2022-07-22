f176897988714e0b21cf2b6b1298a7eb5d7e90b8
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2022-07-21 22:27:21 -0400

    Update commands.bash

--- commands.bash
+++ commands.bash
@@ -1,7 +1,11 @@
 sudo pacman-mirrors --fasttrack && sudo pacman -Syu
 
-sudo pacman -Syu
 
 vim /etc/pacman.conf
 #uncomment 2 lines below this:
 [multilib]
+
+sudo pacman -Sl multilib
+
+sudo pacman -Syu
+sudo pacman -S steam
