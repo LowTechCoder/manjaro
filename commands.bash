e3bbe4f00930792d2f8023484a85bd4d8e37e718
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2022-07-23 13:44:36 -0400

    Update commands.bash

--- commands.bash
+++ commands.bash
@@ -13,3 +13,5 @@ sudo pacman -S steam
 touch .zshrc
 echo 'alias pacup="sudo pacman -Syu"' >> .zshrc
 echo 'alias aup="sudo pamac upgrade --aur"' >> .zshrc
+
+#ffmpeg  -framerate 60 -f x11grab -i :0.0 output.mp4
